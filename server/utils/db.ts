ef3da33d16b0801f4feda0112ee1cd708429f092
Victor-Iroko <irokovictor7@gmail.com>
2025-12-27 12:24:22 +0100

    fix(deployment): :bug: trying to see if its a null value causing the crash

--- server/utils/db.ts
+++ server/utils/db.ts
@@ -1,7 +1,7 @@
 import { drizzle } from "drizzle-orm/bun-sql";
 import * as schema from "../database/schema";
 
-export const db = drizzle(process.env.DATABASE_URL!, {
+export const db = drizzle(process.env.DATABASE_URL || "", {
   schema,
   logger: true,
   casing: "snake_case",
